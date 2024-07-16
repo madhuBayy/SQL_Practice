5350beb899ccb6aa11cb471255f5bb5f8db539b6
Madhulika B <32081283+madhuBayy@users.noreply.github.com>
2024-07-16 20:33:58 +0530

    Create teams_power_users.sql

---
+++ Easy/teams_power_users.sql
@@ -0,0 +1,15 @@
+/*
+
+Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022.
+
+'messages' Table:
+
+| Column Name  |  Type     | 
+|--------------|-----------|
+| message_id   | integer   |
+| sender_id    | integer   |
+| receiver_id  | integer   |
+| content      | varchar   |
+| sent_date    | datetime  |
+
+*/
