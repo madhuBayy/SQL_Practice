f2577e2c9a2fe61273d2b9a1d67dd066f9a808ca
Madhulika B <32081283+madhuBayy@users.noreply.github.com>
2024-07-16 19:57:59 +0530

    Create avg_post_hiatus.sql

---
+++ Easy/avg_post_hiatus.sql
@@ -0,0 +1,16 @@
+/*
+
+Given a table of Facebook posts, for each user who posted at least twice in 2021, write a query to find the number of days
+between each user’s first post of the year and last post of the year in the year 2021.
+
+'posts' Table:
+
+| Column Name  |  Type     | 
+|--------------|-----------|
+| user_id      | integer   |
+| post_id      | integer   |
+| post_content | string    |
+| post_date    | timestamp |
+
+
+*/
