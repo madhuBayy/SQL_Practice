dbc098addf60f7e1b0c21b5d638951ae4869b20d
Madhulika B <32081283+madhuBayy@users.noreply.github.com>
2024-07-20 15:30:53 +0530

    Create avg_review_ratings.sql

---
+++ Easy/avg_review_ratings.sql
@@ -0,0 +1,16 @@
+/*
+
+Given the reviews table, write a query to retrieve the average star rating for each product, grouped by month.
+Sort the output first by month and then by product ID.
+
+'reviews' Table:
+
+| Column Name  |  Type     | 
+|--------------|-----------|
+| user_id      | integer   |
+| review_id    | integer   |
+| submit_date  | datetime  |
+| product_id   | integer   |
+| stars        | integer   |
+
+*/
