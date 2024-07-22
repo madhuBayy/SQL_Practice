7397055adb985a6bcec9f724c58b72622159389d
Madhulika B <32081283+madhuBayy@users.noreply.github.com>
2024-07-22 17:49:47 +0530

    Create well_paid_employees.sql

---
+++ Easy/well_paid_employees.sql
@@ -0,0 +1,15 @@
+/*
+
+Identify all employees who earn more than their direct managers. The result should include the employee's ID and name.
+
+'employee' Table:
+
+| Column Name  |  Type     | 
+|--------------|-----------|
+| employee_id  | integer   |
+| name         | string    |
+| salary       | integer   |
+| department_id| integer   |
+| manager_id   | integer   |
+
+*/
